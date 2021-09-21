02ed1f5e757bf9540816a5d9c81fe7523c8ef32b
dlejay <39823850+dlejay@users.noreply.com>
2021-09-21 23:49:55 +0900

    Add unloading environment.plist & correct spelling

--- uninstall.sh
+++ uninstall.sh
@@ -10,6 +10,14 @@ say(){
 	fi
 }
 
+# Unload environment.plist
+if [ -e ~/Library/LaunchAgents/environment.plist ]
+then
+	launchctl unload ~/Library/LaunchAgents/environment.plist
+	say "Unloaded environment.plist"
+fi
+
+# Clean files
 ENV_FOLDER=${XDG_DATA_HOME:-~/Library/Application Support}/Environment
 
 if [ -d "$ENV_FOLDER" ]
@@ -20,8 +28,8 @@ fi
 
 if [ -d ~/Library/LaunchAgents ]
 then
-	rm -f ~/Library/LaunchAgents/environment.txt
-	say 'Removed ~/Library/LaunchAgents/environment.txt'
+	rm -f ~/Library/LaunchAgents/environment.plist
+	say 'Removed ~/Library/LaunchAgents/environment.plist'
 	rm -f ~/Library/LaunchAgents/environment.readme.txt
 	say 'Removed ~/Library/LaunchAgents/environment.readme.txt'
 fi
